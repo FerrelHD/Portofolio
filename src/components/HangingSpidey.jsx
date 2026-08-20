6a46c46498900a37a3f5dd448cf636cf0d34e2ca
Ferrel Rashad <ferrelrashadakeyla2014gmail.com>
2026-08-20 14:17:17 +0700

    feat: use spideyhanging.gif asset directly in HangingSpidey component

--- src/components/HangingSpidey.jsx
+++ src/components/HangingSpidey.jsx
@@ -1,30 +1,21 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import spideyGif from "../assets/spidey.gif";
+import spideyHangingGif from "../assets/spideyhanging.gif";
 
 const HangingSpidey = () => {
   return (
-    <div className="relative flex flex-col items-center select-none pointer-events-none z-30">
-      {/* Web Line attached right to the top border of the photo frame */}
-      <div
-        className="w-[2px] bg-white/90 shadow-[0_0_8px_rgba(255,255,255,0.8)] animate-pulse"
-        style={{ height: "40px" }}
+    <motion.div
+      animate={{ rotate: [-4, 4, -4] }}
+      transition={{ duration: 3.4, repeat: Infinity, ease: "easeInOut" }}
+      className="relative origin-top select-none pointer-events-none z-30"
+    >
+      <img
+        src={spideyHangingGif}
+        alt="Hanging Spider-Man Easter Egg"
+        className="w-16 h-auto sm:w-20 object-contain mix-blend-multiply drop-shadow-[0_4px_12px_rgba(0,0,0,0.3)]"
       />
-
-      {/* Hanging Upside Down Pixel Spidey with Pendulum Swing */}
-      <motion.div
-        animate={{ rotate: [-6, 6, -6] }}
-        transition={{ duration: 3.2, repeat: Infinity, ease: "easeInOut" }}
-        className="relative -mt-1 origin-top flex flex-col items-center"
-      >
-        <img
-          src={spideyGif}
-          alt="Hanging Spider-Man Easter Egg"
-          className="w-12 h-12 sm:w-14 sm:h-14 object-contain mix-blend-multiply drop-shadow-[0_4px_12px_rgba(255,30,38,0.5)]"
-        />
-      </motion.div>
-    </div>
+    </motion.div>
   );
 };
 
